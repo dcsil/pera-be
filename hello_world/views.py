e3ecff9cb3601bc26484abd0cacfe4a18277b48f
Parth Vats <parthvats02@yahoo.com>
2025-03-01 15:39:34 -0500

    Implement Pytest + add a hello world test

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 def index(request):
-    return HttpResponse("Hello world from Django")
+    return JsonResponse({"message": "Hello world from Django"})
