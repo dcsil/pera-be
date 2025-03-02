72b0c3485bf7db59e30d01738786aad3a3d49ffd
Parth Vats <parthvats02@yahoo.com>
2025-03-02 01:41:50 -0500

    Rebrand to Pera

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### How to run
     $ pip install django
     $ python manage.py runserver
-See [neurolingo\_be/urls.py](neurolingo_be/urls.py).
+See [pera\_be/urls.py](pera_be/urls.py).
--- pera_be/urls.py
+++ pera_be/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for neurolingo_be project.
+URL configuration for pera_be project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
