26291771183a58700ba37bd5bdc4f503d87832f7
Krishna Cheemalalapati <krishkrishct@gmail.com>
2025-03-04 01:06:41 -0500

    Integrate Sentry

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -1,7 +1,19 @@
+import logging as logger
+
 from django.urls import path
+from django.http import HttpResponse
 
 from . import views
 
+def trigger_error(_request):
+    '''
+    Trigger an error to test Sentry
+    '''
+    logger.error("Sentry Backend Example Error")
+    response = HttpResponse("Sentry Backend Example Error", status=500)
+    return response
+
 urlpatterns = [
+    path("sentry-debug/", trigger_error),
     path("", views.index.as_view(), name="index")
 ]
