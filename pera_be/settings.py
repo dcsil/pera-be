06ed6a05f9fc1080092cb6fc8ca1e22a27cf45f8
krishnacheemalapati <45082599+krishnacheemalapati@users.noreply.github.com>
2025-03-04 23:44:18 -0500

    DEV-77: Integrate Sentry (#4)

--- pera_be/settings.py
+++ pera_be/settings.py
@@ -12,6 +12,24 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://b5a3bffaa9576ddc380cb6986b707725@o4508909174718464.ingest.us.sentry.io/4508917862629376",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    _experiments={
+        # Set continuous_profiling_auto_start to True
+        # to automatically start the profiler on when
+        # possible.
+        "continuous_profiling_auto_start": True,
+    },
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
