39a0606849e29bd9f794105c08e9db5d6ac9315c
Parth Vats <parthvats02@yahoo.com>
2025-04-02 04:16:19 -0400

    DEV-83: Create DB locally with docker compose

--- pera_be/settings.py
+++ pera_be/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 import sentry_sdk
 
@@ -48,7 +49,7 @@ ALLOWED_HOSTS = [
     "localhost",
 ]
 
-FRONTEND_URL = "https://pera-dydhd2huhybjakgg.canadacentral-01.azurewebsites.net/"
+FRONTEND_URL = "https://pera-dydhd2huhybjakgg.canadacentral-01.azurewebsites.net"
 
 
 # Application definition
@@ -107,11 +108,16 @@ WSGI_APPLICATION = "pera_be.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default=5432),
     }
 }
+
 
 
 # Password validation
